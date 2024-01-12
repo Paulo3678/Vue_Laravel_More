16ce493d9caf2bfd0cbb81e8c91cd1f61fcd6caf
Paulo <pauloecomercial367@gmail.com>
2024-01-12 19:48:00 -0300

    :sparkles: modal para criar usuario e criacao de usuario

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,8 +1,13 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 
 // ref([]) faz com que a variavel seja reativa
 const users = ref([]);
+const form = reactive({
+    name: '',
+    email: '',
+    password: '',
+});
 
 const getUsers = () => {
     axios.get('/api/users')
@@ -10,10 +15,21 @@ const getUsers = () => {
             users.value = response.data;
         });
 }
-
+const createUser = () => {
+    axios.post('/api/users', form)
+        .then((response) => {
+            // users.value.unshift(response.data) -> Para colocar em primeiro
+            users.value.push(response.data)
+            form.name = '';
+            form.email = '';
+            form.password = '';
+            $('#createUserModal').modal('hide');
+        });
+}
 onMounted(() => {
     getUsers();
 })
+
 </script>
 
 <template>
@@ -36,6 +52,9 @@ onMounted(() => {
 
     <div class="content">
         <div class="container-fluid">
+            <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+                Add New User
+            </button>
             <div class="card">
                 <div class="card-body">
                     <table class="table table-bordered">
@@ -60,6 +79,46 @@ onMounted(() => {
                             </tr>
                         </tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="createUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createUserModalLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form autocomplete="off">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input v-model="form.name" type="text" class="form-control " id="name"
+                                aria-describedby="nameHelp" placeholder="Enter full name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input v-model="form.email" type="email" class="form-control " id="email"
+                                aria-describedby="nameHelp" placeholder="Enter full name">
+                        </div>
+                    </form>
+
+                    <div class="form-group">
+                        <label for="email">Password</label>
+                        <input v-model="form.password" type="password" class="form-control " id="password"
+                            aria-describedby="nameHelp" placeholder="Enter password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button @click="createUser" type="button" class="btn btn-primary">Save</button>
                 </div>
             </div>
         </div>
