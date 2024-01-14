738104b8e185a98c787b4e70f366a94fcb6653bd
Paulo <pauloecomercial367@gmail.com>
2024-01-14 11:19:11 -0300

    :gem: filtrando appointments

--- resources/js/pages/appointments/ListAppointments.vue
+++ resources/js/pages/appointments/ListAppointments.vue
@@ -1,10 +1,18 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
-const appointmentStatus = { 'scheduled': 1, 'confirmed': 2, 'canceld': 3 };
+// const appointmentStatus = { 'scheduled': 1, 'confirmed': 2, 'canceld': 3 };
+const selectedStatus = ref();
+const appointmentStatus = ref([]);
+const getAppointmentStatus = () => {
+    axios.get('/api/appointment-status')
+        .then((response) => {
+            appointmentStatus.value = response.data;
+        });
+}
 const appointments = ref([]);
 const getAppointments = (status) => {
-
+    selectedStatus.value = status;
     const params = {};
     if (status) {
         params.status = status;
@@ -15,9 +23,13 @@ const getAppointments = (status) => {
         appointments.value = response.data;
     })
 }
+const appointmentsCount = computed(() => {
+    return appointmentStatus.value.map(status => status.count).reduce((acc, value) => acc + value, 0);
+});
 
 onMounted(() => {
     getAppointments();
+    getAppointmentStatus();
 });
 </script>
 
@@ -51,28 +63,20 @@ onMounted(() => {
                             </a>
                         </div>
                         <div class="btn-group">
-                            <button @click="getAppointments()" type="button" class="btn btn-secondary">
+                            <button @click="getAppointments()" type="button" class="btn"
+                                :class="[typeof selectedStatus === 'undefined' ? 'btn-secondary' : 'btn-default']">
                                 <span class="mr-1">All</span>
-                                <span class="badge badge-pill badge-info">0</span>
+                                <span class="badge badge-pill badge-info">{{ appointmentsCount }}</span>
                             </button>
 
-                            <button @click="getAppointments(appointmentStatus.scheduled)" type="button"
-                                class="btn btn-default">
-                                <span class="mr-1">Scheduled</span>
-                                <span class="badge badge-pill badge-primary">0</span>
+                            <button v-for="status in appointmentStatus" @click="getAppointments(status.value)" type="button"
+                                class="btn" :class="[selectedStatus === status.value ? 'btn-secondary' : 'btn-default']">
+                                <span class="mr-1">{{ status.name }}</span>
+                                <span class="badge badge-pill" :class="`badge-${status.color}`">
+                                    {{ status.count }}
+                                </span>
                             </button>
 
-                            <button @click="getAppointments(appointmentStatus.confirmed)" type="button"
-                                class="btn btn-default">
-                                <span class="mr-1">Confirmed</span>
-                                <span class="badge badge-pill badge-success">0</span>
-                            </button>
-
-                            <button @click="getAppointments(appointmentStatus.canceld)" type="button"
-                                class="btn btn-default">
-                                <span class="mr-1">Cancelled</span>
-                                <span class="badge badge-pill badge-danger">0</span>
-                            </button>
                         </div>
                     </div>
                     <div class="card">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AppointmentController;
+use App\Http\Controllers\Admin\AppointmentStatusController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
@@ -32,6 +33,7 @@ Route::delete('/api/users', [UserController::class, 'bulkDelete']);
 Route::patch('/api/users/{user}/chage-role', [UserController::class, 'changeRole']);
 
 Route::get('/api/appointments', [AppointmentController::class, 'index']);
+Route::get('/api/appointment-status', [AppointmentStatusController::class, 'getStatusWithCount']);
 
 
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
